8c8deb177004f747d38edb6eeb0e6048a389bdf2
Inna Stepanova <inna-panichkina@yandex.ru>
2020-10-19 21:52:21 +0300

    Initial Commit

---
+++ HomeWork3.2/HomeWork3_2App.swift
@@ -0,0 +1,17 @@
+//
+//  HomeWork3_2App.swift
+//  HomeWork3.2
+//
+//  Created by Инна Степанова on 19.10.2020.
+//
+
+import SwiftUI
+
+@main
+struct HomeWork3_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
